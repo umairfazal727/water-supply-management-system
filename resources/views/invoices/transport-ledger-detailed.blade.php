1c62a6dde8a4ed13462cd3c590b312f1612709d8
umairfazal727 <umairfazal727@gmail.com>
2025-10-31 08:05:26 +0500

    update ledger

--- resources/views/invoices/transport-ledger-detailed.blade.php
+++ resources/views/invoices/transport-ledger-detailed.blade.php
@@ -41,8 +41,8 @@
                     <td>{{ $d->delivery_date?->format('M d, Y') }}</td>
                     <td>{{ $d->delivery_number }}</td>
                     <td>{{ $d->deliveryCustomer?->name }}</td>
-                    <td>{{ $d->order?->vehicle?->vehicle_number ?? 'N/A' }}</td>
-                    <td>{{ $d->order?->driver?->name ?? 'N/A' }}</td>
+                    <td>{{ $d->order?->vehicle_number ?? 'N/A' }}</td>
+                    <td>{{ $d->order?->name ?? 'N/A' }}</td>
                     <td>{{ $d->customer_site ?? 'N/A' }}</td>
                     <td>{{ $d->customer_location ?? 'N/A' }}</td>
                     <td>{{ $d->trip_size }} gal</td>
